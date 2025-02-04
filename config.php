92b1b74cea8fb1ffa27f703265942b93d9d4f22c
Ariane . <lelisariane.2@gmail.com>
2025-02-04 11:56:22 +0800

    Updated PHP logic and fixed database connection

--- config.php
+++ config.php
@@ -1,6 +1,12 @@
 <?php
-$db = mysqli_connect("localhost", "root", "", "todo_db");
-if (!$db) {
-    die("Connection failed: " . mysqli_connect_error());
+$dsn = 'pgsql:host=dpg-cugoj62j1k6c73b07urg-a.oregon-postgres.render.com;port=5432;dbname=todoapp_g77m';
+$user = 'todoapp_g77m_user';
+$password = 'MJovArDsluu5fbHq1ww6SzLD2wADkCAq';
+
+try {
+    $pdo = new PDO($dsn, $user, $password);
+    echo "Connected to the database!";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
 }
 ?>
